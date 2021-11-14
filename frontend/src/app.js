24747f9980d7e8aad6e6b3ac2b41c977404093c3
_nahoy <42898627+NahoyGames@users.noreply.github.com>
2021-11-14 16:54:48 -0500

    Slidedeck layout

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -4,9 +4,18 @@ import Border from './border'
 
 const View = (
     <Border class="container">
-        <section class="child" style={{ backgroundColor: 'blue' }}>
-            <img src="res/donkey.png" class="party-animal"></img>
-            <img src="res/joe.png" class="candidate-bust"></img>
+        <section class="child" style={{ backgroundColor: '#3c5791' }}>
+            {/* <img src="res/donkey.png" class="party-animal"></img>
+            <img src="res/joe.png" class="candidate-bust"></img> */}
+            {/* <div class="content">
+                Hey!
+            </div> */}
+            <section class="content" style={{backgroundColor: 'green'}}>
+                Hello!
+            </section>
+            <section class="content" style={{backgroundColor: 'violet'}}>
+                World!
+            </section>
         </section>
         <section class="child" style={{ backgroundColor: 'red' }}> Hi! </section>
         <section class="child" style={{ backgroundColor: 'violet' }}> Hi! </section>
--- frontend/src/border.js
+++ frontend/src/border.js
@@ -26,6 +26,8 @@ export default class Border
             overflow: 'scroll',
 
             fontSize: '0',
+
+            backgroundColor: '#1f242b'
         }
     }
 }
